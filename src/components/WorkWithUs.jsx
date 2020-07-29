7b49272379c2cc976385f26f5e5bccc47b944c92
Ivan Morales <ivann_mg@hotmail.com>
2020-07-29 13:12:31 -0500

    WorkWithUs Component

--- src/components/WorkWithUs.jsx
+++ src/components/WorkWithUs.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 
+import "./WorkWithUs.css";
+import people from "../img/Group 4040.png";
+import arrow from "../img/arrow1.png";
+import Button from "./Button";
+import { Col, Container, Row } from "react-bootstrap";
+
 function WorkWithUs() {
   return (
-    <div className="info">
-      <h1 className="info-h1">¡Te encantará!</h1>
-      <h1 className="info-h1">
-        <span>trabajar con nosotros!</span>
-      </h1>
+    <div id="workingWithUs">
+      <div className="info">
+        <h1 className="info-h1">¡Te encantará!</h1>
+        <h1 className="info-h1">
+          <span className="orange">trabajar con nosotros!</span>
+        </h1>
+      </div>
+      <div>
+        <img src={people} alt="people" class="peopleImg" />
+        <Container>
+          <Row>
+            <Col>
+              <p>Contratación remota</p>
+            </Col>
+            <Col>
+              <img src={arrow} alt="arrow" className="arrow-right" />
+            </Col>
+            <Col md="auto">
+              <p>Entrevista con el área de RH</p>
+            </Col>
+            <Col>
+              <img src={arrow} alt="arrow" className="arrow-right" />
+            </Col>
+            <Col>
+              <p>Prueba práctica</p>
+            </Col>
+            <Col>
+              <img src={arrow} alt="arrow" className="arrow-right" />
+            </Col>
+            <Col>
+              <p>Entrevista técnica</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="genericBtn">
+        <Button />
+      </div>
     </div>
   );
 }
