4e31f0bcb9c8effc206cc5aa77df802df5fd96fa
Ivan Morales <ivann_mg@hotmail.com>
2020-07-29 15:12:10 -0500

    Form Component: cropped image

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+import "./Form.css";
+import { Navbar } from "react-bootstrap";
+import Logo from "./Logo";
+
 function Form() {
   return (
-    <div>
-      <h1>Form!</h1>
+    <div id="form">
+      <Navbar>
+        <Navbar.Brand href="#home">
+          <Logo />
+        </Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
